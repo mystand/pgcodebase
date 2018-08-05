c9b419827f36a1e6bec25432bb2d2557ddb34747
Andrew Balakirev <balakirev.andrey@gmail.com>
2018-08-05 19:30:05 +0300

    Ability to set connection-string and current-schema

--- cli.js
+++ cli.js
@@ -11,6 +11,8 @@ commander
   .option('-p, --password <password>', 'Postgresql password')
   .option('-d, --database <database>', 'Postgresql database name')
   .option('-P, --port <port>', 'Postgresql port')
+  .option('-C, --connection-string <connectionString>', 'Postgresql connection string')
+  .option('-s, --current-schema <currentSchema>', 'Default postgresql schema to use')
   .option('-D, --dir <dir>', 'Sql files directory')
   .option('-c, --create-only', 'Only create entities in database, drop nothing')
   .option('-r, --drop-only', 'Only drop entities in database, create nothing')
@@ -23,6 +25,8 @@ pgcodebase({
   database: commander.database || process.env.PGDATABASE,
   port: commander.port || process.env.PGPORT,
   dir: commander.dir || process.env.PGCODEBASE_DIR,
+  connectionString: commander.connectionString || process.env.PGCODEBASE_CONNECTION_STRING,
   createOnly: commander.createOnly,
-  dropOnly: commander.dropOnly
+  dropOnly: commander.dropOnly,
+  currentSchema: commander.currentSchema
 }).catch(console.error)
